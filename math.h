caf9dee660dba8dbbb481c8dbf3067793cbec26b
Israel <israel.toledo@iteso.mx>
2022-10-12 11:03:12 -0500

    added function doubleFloat

--- math.h
+++ math.h
@@ -2,6 +2,8 @@
 
 #define MAX_INT (2^sizeof(int))/2
 #define MAX_DOUBLE (2^sizeof(double))/2
+#define MAX_FLOAT (2^sizeof(float))/2
 
 int doubleInt(int a);
 int doubleDouble(double a);
+int doubleFloat(float a);
